8d4667b55f4bdb5bc3ecb79af0dabb20f89436c3
Alejandro <ramirezaed@gmail.com>
2026-08-08 15:04:58 -0300

    feat: crea migraciones para carrito y carrito_items

---
+++ database/migrations/2026_08_08_175955_create_cart_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            //cascadaOndelete-> cuando se borra un carrito se borra el cart_items
+            $table->foreignId('cart_id')->constrained('carts')->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained('products');
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+            //dentro de un carrito el un cart_id o producto_id solo puede aparecer una vez
+            $table->unique(['cart_id', 'product_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
