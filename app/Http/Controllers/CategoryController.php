c207975dd1d7d1e420a9306cdbc5cb9fcc6da349
Alejandro <ramirezaed@gmail.com>
2026-08-09 20:59:08 -0300

    agrega controlador para registrar una nueva categoria

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Contracts\Support\ValidatedData;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    //funcion para registrar una nueva categoria
+    public function register(Request $request)
+    {
+        $validateDate = $request->validate([
+            //verifica que que el nombre sea string y que no este registrado en la bd
+            "name" => "required|string|unique:categories"
+        ]);
+        try {
+            $category = Category::create($validateDate);
+            return response()->json($category, 201);
+        } catch (\Exception $e) {
+            return response()->json(["message" => "error interno al intentar registrar una nueva categoria"], 500);
+        }
+    }
+}
