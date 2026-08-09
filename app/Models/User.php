e5ad1d9fc04c57dc71ee03a150b6f779881b154a
Alejandro <ramirezaed@gmail.com>
2026-08-09 18:31:16 -0300

    feat: agregar modelo usuer y su relacion con cart

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,23 +10,30 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+//se definen los valores para guardar de forma masiva
+#[Fillable(['name', 'email', 'password', 'role'])]
+//se definen los campos ocultos, no se envian en la respuesta
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+    // define el nombre en plural de la tabla que este modelo debe uasr
+    protected $table = 'users';
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    //Los atributos que deben ser convertidos a tipos de datos específicos.
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    //se define la relacion con la clase modelo
+    //hasOne relacion 1<->1
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
     }
 }
