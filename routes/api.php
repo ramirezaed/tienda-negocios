953bf67cbbb95824c12759aaed0d031b054efde2
Alejandro <ramirezaed@gmail.com>
2026-08-10 00:29:49 -0300

    fix: corrige tipo de datos en productos

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
@@ -22,7 +23,7 @@ Route::prefix('users')->group(function () {
 //todas estas rutas va a estar en http://127.0.0.1:8000/api/categories/....
 Route::prefix('categories')->group(function () {
     Route::get('/', [CategoryController::class, 'index']);          // lista de categorias
-    Route::post('/register', [CategoryController::class, 'register']); // registrar categoria
+    Route::post('/', [CategoryController::class, 'register']); // registrar categoria
     Route::get('/{id}', [CategoryController::class, 'getById']);    // buscar por ID
     Route::put('/{id}', [CategoryController::class, 'update']);     // modificar por ID
     Route::delete('/{id}', [CategoryController::class, 'delete']);  // eliminar por ID
@@ -30,8 +31,17 @@ Route::prefix('categories')->group(function () {
 
 // rutas productos
 Route::prefix('products')->group(function () {
-    Route::post('/register', [ProductController::class, 'register']); // registrar
+    Route::post('/', [ProductController::class, 'register']); // registrar
     Route::get('/{id}', [ProductController::class, 'getById']);        //buscar por id
     Route::put('/{id}', [ProductController::class, 'update']);         // modificar por ID
     Route::delete('/{id}', [ProductController::class, 'delete']);       // eliminar por ID
+});
+
+// rutas cart
+Route::prefix('cart')->group(function () {
+    Route::get('/', [CartController::class, 'index']);             // ver el carrito
+    Route::post('/add', [CartController::class, 'addProduct']);    // Agregar  producto
+    Route::post('/clear', [CartController::class, 'clear']);       // Vaciar carrito
+    Route::post('/remove', [CartController::class, 'removeProduct']); // quitar un producto
+    Route::delete('/delete', [CartController::class, 'deleteCart']); // eliminar carrito
 });
