d1a3e797899c657bdff0e7d54afe919545ecfe18
Alejandro <ramirezaed@gmail.com>
2026-08-09 22:29:19 -0300

    fix : agregar controlador para obtner lista de prodcutos, y controlador para obtener un producto por id

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
 
 class ProductController extends Controller
 {
@@ -19,9 +20,31 @@ class ProductController extends Controller
         ]);
         try {
             $producto = Product::create();
-            return response()->json($producto);
+            return response()->json($producto, 201);
         } catch (\Exception $e) {
-            return response()->json(["message" => "error interno al intentar registrar una nueva categoria"], 500);
+            return response()->json(["message" => "error interno al intentar registrar un nuevo producto"], 500);
+        }
+    }
+
+    public function index()
+    {
+        try {
+            $product = Product::all();
+            return response()->json($product, 200);
+        } catch (\Exception $e) {
+            return response()->json(["message" => "error interno al intentar obtener lista de productos"], 500);
+        }
+    }
+    public function getById(int $id)
+    {
+        try {
+            $product = Product::find($id);
+            if ($product) {
+                return response()->json(["message" => "producto no encontrado"], 404);
+            }
+            return response()->json($product, 200);
+        } catch (\Exception $e) {
+            return response()->json(["message" => "error interno al intentar obtener datos del producto"], 500);
         }
     }
 }
