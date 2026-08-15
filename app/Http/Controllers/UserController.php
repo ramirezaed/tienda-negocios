341019522985c032af72b48f45da38a20e5c9600
Alejandro <ramirezaed@gmail.com>
2026-08-15 20:01:48 -0300

    refactor: cambiar firma en los controlader de usuario, cambia id por user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,14 +6,13 @@ use App\Http\Requests\AddUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use App\service\UserService;
+use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
 {
-    //inyecta el servicio en el contructor
-    //constructor se ejecuta automaticamente 
-    public function __construct(private UserService $userService) {}
+
     //funcion para mostrsr todos los usuarios
-    public function index()
+    public function index(): JsonResponse
     {
         //muestra lista de usuarios, de 10 en 10
         $users = User::paginate(10);
@@ -22,35 +21,30 @@ class UserController extends Controller
 
     //funcion para registrar un usuario
     //store es el nonbre standar para registrar
-    public function store(AddUserRequest $request)
+    public function store(AddUserRequest $request): JsonResponse
     {
         $user = User::create($request->validated());
         return Response()->json($user, 201);
     }
 
-    //funcion  para modificar un usuario
-    public function update(UpdateUserRequest $request, int $id)
+    //funcion para obtener un los datos de un usuario
+    public function show(User $user): JsonResponse
     {
-        //llama al servicio findbyid
-        $user = $this->userService->findById($id);
+        return response()->json($user);
+    }
+
+    //funcion  para modificar un usuario
+    public function update(UpdateUserRequest $request, User $user)
+    {
         //si el usuario existe valida los datos, actualiza el registro
         $user->update($request->validated());
         return response()->json($user, 200);
     }
 
     //funcion para elimiar un usuario
-    public function destroy(int $id)
+    public function destroy(User $user): JsonResponse
     {
-        //lama al servicio buscar por id
-        $user = $this->userService->findById($id);
         $user->delete();
         return response()->json(["message" => "usuario eliminado con exito"], 200);
-    }
-
-    //funcion para obtener un los datos de un usuario
-    public function show(int $id)
-    {
-        $user = $this->userService->findById($id);
-        return response()->json($user);
     }
 }
