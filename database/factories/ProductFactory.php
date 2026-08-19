0b04d57f4240c67d465456c76ba5e2f12101de1b
Alejandro <ramirezaed@gmail.com>
2026-08-19 13:34:56 -0300

    feat: agrega productFactory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words(3, true),
+            'description' => fake()->sentence(10),
+            'price' => fake()->randomFloat(2, 500, 50000),
+            'stock' => fake()->numberBetween(0, 100),
+            'category_id' => Category::inRandomOrder()->first()->id,
+        ];
+    }
+}
