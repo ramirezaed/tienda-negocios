10e23dab2d019f4d08bcbb2559906a7b5c03b91c
Alejandro <ramirezaed@gmail.com>
2026-08-19 13:43:48 -0300

    agrega seeder y factory para users, category, product

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //20 es la cantidad de productos que se crean
+        Product::factory(20)->create();
+    }
+}
