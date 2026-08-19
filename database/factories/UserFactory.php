b4001c854cc7e56b53eb8eb063c65a67460ab689
Alejandro <ramirezaed@gmail.com>
2026-08-19 13:35:47 -0300

    feat: agrega userFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,11 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => fake()->randomElement([
+                'admin',
+                'employee',
+                'manager',
+            ]),
             'remember_token' => Str::random(10),
         ];
     }
@@ -38,7 +43,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
