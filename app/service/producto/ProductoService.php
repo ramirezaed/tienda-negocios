57297719e8685ed1f7ee90fc9cc3f0760984be0e
Alejandro <ramirezaed@gmail.com>
2026-08-22 12:42:17 -0300

    feat: agrega servicio y dto para modificar producto

--- app/service/producto/ProductoService.php
+++ app/service/producto/ProductoService.php
@@ -3,6 +3,7 @@
 namespace App\service\producto;
 
 use App\DTO\Products\CreateProductDTO;
+use App\DTO\Products\UpdateProductDTO;
 use App\Models\Product;
 
 class ProductService
@@ -11,5 +12,17 @@ class ProductService
     public function create(CreateProductDTO $data): Product
     {
         return Product::create($data->toArray());
+    }
+
+    public function update(UpdateProductDTO $data, Product $product): Product
+    {
+        //si no hay cambios devuelve el producto
+        if (!$data->hasChanges()) {
+            return $product;
+        }
+        //actualiza el producto
+        $product->update($data->toArray());
+        //devuelve el producto actualizado
+        return $product
     }
 }
